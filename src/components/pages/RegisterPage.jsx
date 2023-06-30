114f1d733f1e91adc7c9f544003c286558b07f9a
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-06-30 10:33:12 +0300

    ami ne raboteshe...

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -64,11 +64,7 @@ const RegisterComponent = ({ errors, messages }) => {
         }
       })
       .catch((err) => {
-        console.error(
-          JSON.stringify(
-            err.response.data.erros.map((err) => err.msg).join("\n")
-          )
-        );
+        console.error(JSON.stringify(err.response.data.erros));
       });
   };
 
