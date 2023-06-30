5f33f0936b2312883242285652ec16e44d2daf6c
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-06-30 15:24:04 +0300

    post author in saved posts

--- src/components/pages/SavedPosts.jsx
+++ src/components/pages/SavedPosts.jsx
@@ -127,6 +127,9 @@ const SavedPosts = () => {
                     <Typography variant="body2" color="text.secondary">
                       {post.description}
                     </Typography>
+                    <Typography gutterBottom variant="h6" component="div">
+                      Author: {post.author}
+                    </Typography>
                   </CardContent>
                 </CardActionArea>
               </Card>
