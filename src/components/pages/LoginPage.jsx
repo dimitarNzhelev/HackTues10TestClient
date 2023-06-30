2a1f8efd5ef2642b5badf1d9a228ef13c79059a0
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-07-01 00:04:57 +0300

    add login error handling

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -52,7 +52,10 @@ const LoginComponent = ({ errors, messages }) => {
     axios
       .post(urlWithProxy, data, { withCredentials: true })
       .then((res) => {
-        console.log(res.data);
+        console.log(res.data.response.data.message);
+        if (res.data.response.data.message) {
+          alert(res.data.response.data.message);
+        }
         if (res.data.user.isverified) {
           navigate("/dashboard");
         } else {
@@ -70,24 +73,6 @@ const LoginComponent = ({ errors, messages }) => {
   return (
     <Container className="d-flex flex-column align-items-center justify-content-center vh-100 py-3 text-white">
       <h1 className="mb-5">Login</h1>
-      {errors &&
-        errors.map((error, index) => (
-          <Alert key={index} variant="danger" className="w-50">
-            {error.message}
-          </Alert>
-        ))}
-
-      {messages && messages.success_msg && (
-        <Alert variant="success" className="w-50">
-          {messages.success_msg}
-        </Alert>
-      )}
-
-      {messages && messages.error && (
-        <Alert variant="danger" className="w-50">
-          {messages.error}
-        </Alert>
-      )}
 
       <Form onSubmit={onSubmit} className="w-50">
         <Form.Group controlId="formEmail">
@@ -120,7 +105,7 @@ const LoginComponent = ({ errors, messages }) => {
           {loading ? "Loading..." : "Login"}
         </Button>
         <Form.Text className="text-light mt-3">
-          Don't have an account?{" "}
+          Don't have an account?
           <a href="/auth/register" className="text-primary">
             Register
           </a>
