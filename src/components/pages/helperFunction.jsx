08c7146ffe59050be2297f5c0223378d01ea65e6
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-07-02 10:44:44 +0300

    mahnah komentarite na helper funkciqta

--- src/components/pages/helperFunction.jsx
+++ src/components/pages/helperFunction.jsx
@@ -14,18 +14,14 @@ export default function useWindowSize() {
       });
     }
 
-    // Run the function once to get the initial window size
     handleResize();
 
-    // Subscribe to window resize events
     window.addEventListener("resize", handleResize);
 
-    // Clean up function
     return () => {
-      // Unsubscribe from window resize events
       window.removeEventListener("resize", handleResize);
     };
-  }, []); // Empty dependency array means this effect will only run once, like componentDidMount
+  }, []);
 
   return windowSize;
 }
