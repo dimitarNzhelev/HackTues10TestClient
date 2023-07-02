a754b20b6ec32dfa34ed2cc245f9ba9108c751b5
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-07-02 18:18:21 +0300

    add margin

--- src/components/pages/LargeScreenPostPage.jsx
+++ src/components/pages/LargeScreenPostPage.jsx
@@ -323,9 +323,7 @@ const LargeScreenPostPage = () => {
             <div
               className="col-lg-6 col-md-6 col-sm-12"
               style={{ width: "100%" }}>
-              <div
-                className="container textContainer"
-                style={{ marginTop: 20 }}>
+              <div className="container textContainer" style={{ margin: 20 }}>
                 <div>
                   <p style={{ color: "white" }}>Description:</p>
                   <p style={{ color: "white" }}>{post.description}</p>
